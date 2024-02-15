adc383a9aeb41c4dc5271e6057e2151a16093688
Lee <94841127+galaxian@users.noreply.github.com>
2024-02-15 12:52:48 +0900

    feat: 유효성 검사 및 exception handler

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.example.student.controller;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.student.dto.ApiResponse;
 import com.example.student.dto.StudentResponseDto;
+import com.example.student.exception.InvalidGradeException;
 import com.example.student.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,11 @@ public class StudentController {
 
 	private ApiResponse<StudentResponseDto> makeResponse(StudentResponseDto responseDto) {
 		return new ApiResponse<>(responseDto);
+	}
+
+	@ExceptionHandler(InvalidGradeException.class)
+	public ApiResponse exceptionHandler(InvalidGradeException invalidGradeException) {
+		return new ApiResponse<>(invalidGradeException.getErrorCode().getCode(), invalidGradeException.getMessage(),
+			invalidGradeException.getData());
 	}
 }
--- src/main/java/com/example/student/domain/Student.java
+++ src/main/java/com/example/student/domain/Student.java
@@ -1,15 +1,24 @@
 package com.example.student.domain;
 
+import static com.example.student.exception.ErrorCode.*;
+
+import com.example.student.exception.InvalidGradeException;
+
 import lombok.Getter;
 
 @Getter
 public class Student {
+
+	private final static int MAX_GRADE = 6;
 
 	private Long id;
 	private String name;
 	private int grade;
 
 	private Student(String name, int grade) {
+		if (grade >= MAX_GRADE) {
+			throw new InvalidGradeException(Bad_Request, "grade는 6이상 입력할 수 없습니다.", MAX_GRADE);
+		}
 		this.name = name;
 		this.grade = grade;
 	}

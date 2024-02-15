1c60642440a88860e13177213b5199553cc0a2f5
Lee <94841127+galaxian@users.noreply.github.com>
2024-02-15 14:12:02 +0900

    feat: apiResponse 출력 조건 추가

--- src/main/java/com/example/student/dto/ApiResponse.java
+++ src/main/java/com/example/student/dto/ApiResponse.java
@@ -1,9 +1,12 @@
 package com.example.student.dto;
 
 import static com.example.student.exception.ErrorCode.*;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
 
 import java.util.Collections;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +15,11 @@ import lombok.Getter;
 public class ApiResponse<T> {
 
 	private Status status;
+	@JsonInclude(NON_EMPTY)
 	private MetaData metaData;
+	@JsonInclude(NON_EMPTY)
 	private List<T> result;
+	@JsonInclude(NON_EMPTY)
 	private Object data;
 
 	public ApiResponse(List<T> result) {
